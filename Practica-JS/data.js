ceb20966e3db0002246419dba96661d788a6ca88
jeremyda173 <jh4017034@gmail.com>
2024-01-22 22:48:07 -0400

    feat: add file data.js for the practice

---
+++ Practica-JS/data.js
@@ -0,0 +1,41 @@
+console.log("Bienvenidos");
+
+// Aprendiendo a optener los datos
+// var data ={
+//     name:"Juan",
+//     age:25,
+//     email:"juanprueba21@gmail.com",
+//     password:"qwerty123456"
+// }
+// console.log(data);
+function OptenData() {
+    var userName = document.getElementById('name').value;
+    var userAge = document.getElementById('age').value;
+    var userEmail = document.getElementById('email').value;
+    var userPassword = document.getElementById('password').value;
+
+    var userInfo = {
+        name: userName,
+        age: userAge,
+        email: userEmail,
+        pass: userPassword
+    };
+
+
+    // Enviar datos al servidor usando Fetch API
+    fetch('http://localhost:3000/guardarDatos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userInfo)
+    })
+    .then(response => response.text())
+    .then(data => {
+        console.log(data);
+        // Puedes agregar aquí cualquier lógica adicional después de guardar los datos en el servidor
+    })
+    .catch(error => {
+        console.error('Error al enviar datos al servidor:', error);
+    });
+}
