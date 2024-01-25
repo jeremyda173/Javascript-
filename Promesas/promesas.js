8a3532cc56f960f7221c6458d70ff3539e43e524
jeremyda173 <jh4017034@gmail.com>
2024-01-25 12:06:40 -0400

    fix: modify promise

--- Promesas/promesas.js
+++ Promesas/promesas.js
@@ -1,2 +1,43 @@
-console.log("Promess");
+console.log("Welcome to Promise");
+
+const Data = [{
+    name: "John",
+    age: 32,
+    }, {
+        name: "Jane",
+        age: 45
+    }, {
+        name: "Mark",
+        age: 18
+}];
+
+// console.log(Data);
+
+
+// const getData=()=>{
+//     return Data;
+// }
+// console.log(getData());
+
+
+const getData = () => {
+    return new Promise((resolve, _reject) => {
+        setTimeout(()=>{
+            resolve(Data);
+            }, 2000);
+        });
+}
+getData().then((Data) => console.log(Data) + console.log("Finally we got the data"));
+
+console.log(getData());
+
+
+// console.log("Inicio de la operación");
+
+// setTimeout(() => {
+//   console.log("Operación asíncrona completada después de 2 segundos");
+// }, 2000);
+
+// console.log("Fin de la operación principal");
+
 
