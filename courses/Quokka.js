6def26390e578bba335b6021c3dd528424cde9f5
jeremyda173 <jh4017034@gmail.com>
2024-07-17 21:16:24 -0400

    feat: add new change

---
+++ courses/Quokka.js
@@ -0,0 +1,18 @@
+
+console.log("Hello world!");
+console.log("Starting");
+document.write("Estamos desde JavaScript ");
+
+const server = 45;
+console.log(server);
+
+const person ={
+    name: 'Jeremy Smith',
+    age: 19,
+    number: '+1 000-000-0000'
+}
+
+console.log(person);
+document.write(server + ' ');
+document.write('<br>');
+document.write(person.name + " " + person.age + " " + person.number)
