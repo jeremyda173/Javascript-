b1ecd249bdd0fe7166e57f87b4105d2b05b55fae
jeremyda173 <jh4017034@gmail.com>
2024-07-19 13:05:12 -0400

    feat: create new formulary

--- courses/prueba.js
+++ courses/prueba.js
@@ -1,6 +1,25 @@
-console.log("Starting");
+// console.log("Starting");
+// // const server = 45;
+// // console.log(server);
 
-const server = 45;
+// function ModalData(){
+//     const telefono = document.getElementById('telefono').value;
+//     const edad = document.getElementById('edad').value;
+//     const fecha = document.getElementById('fecha').value;
 
-console.log(server);
+//     document.getElementById('telefonoInModal').innerText = 'Teléfono: ' + telefono;
+//     document.getElementById('edadInModal').innerText = 'Edad: ' + edad;
+//     document.getElementById('fechaInModal').innerText = 'Fecha: ' + fecha;
+
+//     document.getElementById('myModal').style.display = 'block';
+// }
+//     function cerrarModal() {
+//         document.getElementById('myModal').style.display = 'none';
+//       }
+
+//       window.onclick = function(event) {
+//         if (event.target == document.getElementById('myModal')) {
+//           cerrarModal();
+//         }
+//     }
 
