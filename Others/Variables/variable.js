991aec07515a98e2d6f30de0b158e5e41f00b947
jeremyda173 <jh4017034@gmail.com>
2024-07-20 18:21:16 -0400

    chore: organize project structure

---
+++ Others/Variables/variable.js
@@ -0,0 +1,15 @@
+var nombre = 'Jeremy'; //  Declaracion de una variable
+let Nombre  = "Maria"; // Declaracion de una variable unica
+console.log(nombre)
+
+var x = 6;
+var y = 4;
+
+var suma = x + y;
+console.log(suma)
+
+var NOMBRE = "Benedeto";
+var EDAD = "32";
+var DATOS = `mi nombre es ${NOMBRE} y tengo ${EDAD} años`;
+console.log(DATOS)
+
