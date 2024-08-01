03ed7c9e138be2366d31fd9b5c1627bb3287d080
jeremyda173 <jh4017034@gmail.com>
2024-08-01 09:50:22 -0400

    docs: started a JavaScript course

---
+++ 176/js/test.js
@@ -0,0 +1,73 @@
+console.log("Welcome my first course in JavaScript world!");
+
+let peopleCourse = 34;
+peopleCourse = true; 
+// CamelCase
+console.log(peopleCourse);
+
+
+const NAME = "Jeremy Smith";
+//typeof es para que me diga que tipo de valor es... String, Number or Boolean
+console.log(NAME);
+
+const Edad = "Edad";
+const Numero = "Numero";
+
+console.log(Edad + " " + Numero);
+
+// `` alt derecho mas 96 para esas comillas
+console.log(`Welcome ${NAME}, You have ${Edad} and your cellphone ${Numero}`);
+
+let num1 = 200
+let num2 = 0
+let num3 = 0
+
+console.log(num1 - num2 / num3);
+// NaN no es un Numero
+console.log((num1 - num2) / num3);
+
+let a = 3
+let b = 5
+let c = 3
+
+c = a + b
+console.log(c);
+c = ++c;
+console.log(c);
+c += a;
+console.log(c);
+
+// Funciones
+function FirstFunction() {
+    console.log("First Function");
+    const Compra = "BMW"
+    return Compra;
+}
+console.log(FirstFunction());
+
+function SecondFunction(i, e) {
+    console.log("Second Function");
+    return `Anoche fui a domir ${i} y me ${e}`;
+}
+console.log(SecondFunction("con mi novia","pillaron"));
+
+const TheFunction = function (a, b) {
+    console.log("The Function in to Variable");
+    return a + b;
+}
+console.log(TheFunction(4, 76));
+
+// Functions flechas
+const TheFunctionFlecha = (a, b) => {
+    return a + b;
+}  
+console.log(TheFunctionFlecha(3,1));
+
+// export default function LlamarJaimito(){
+
+//     return(
+
+//     );
+// }
+
+FirstFunction();
