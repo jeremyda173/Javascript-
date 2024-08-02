c0a9e1818d89da840b63a53ab31f4157c5d30fb3
jeremyda173 <jh4017034@gmail.com>
2024-08-02 13:45:47 -0400

    docs: making progress in the JavaScript course

---
+++ 176/Eje/1.js
@@ -0,0 +1,10 @@
+              {/* // Has hecho una compra y sabes el precio del producto y su iva. Haz un script que te calcule el precio total que vas a pagar por tu compra. Te recuerdo que para calcular el total debes sumar al precio el resultado de multiplicar precio por el iva y dividir por 100.
+              //   Precio 200€
+              //   Iva: 21%
+              //   El total son 242 €.  // 200 + 21*200/100 */}
+
+            const precio = 200
+            const iva = 0.21
+            const total = 200 + (21*200)/100
+            const PrecioTotal = total
+            console.log(total);
