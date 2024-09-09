50598e594091b53975343fd8f45ece77cef0e0f2
jeremyda173 <jh4017034@gmail.com>
2024-09-09 09:23:42 -0400

    chore: continue progress on course

---
+++ 176/js/test3.js
@@ -0,0 +1,28 @@
+// Episodio 59 en adelante
+
+const miArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+console.log(miArray);
+for (let i = 0; i < miArray.length; i++){
+    const miArray2 = miArray[i].reverse();
+    console.log(miArray2);
+}
+for (let i = 0; i < miArray.length; i++){
+    console.log(miArray[0]);
+    console.log(miArray[1]);
+    console.log(miArray[2]);
+}
+
+const m = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+for (let i = 0; i < miArray.length; i++){
+    console.log(m[i]);
+}
+
