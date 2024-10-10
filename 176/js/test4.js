7565baa21cedab00f197b561b00a542f18fb8060
jeremyda173 <jh4017034@gmail.com>
2024-10-10 17:54:15 -0400

    chore: continue progress on course 73

--- 176/js/test4.js
+++ 176/js/test4.js
@@ -1,1 +1,11 @@
-// Episodio 72 en adelante
+// Episodio 73 en adelante
+
+// ----------- Ejercicio 6 ------------
+// Crear una funcio que reciba un array con datos y me regrese unos de estos datos de forma aleatoria
+
+function checkArray(cadena){
+const random = Math.floor(Math.random() * cadena.length);
+    return cadena[random];
+}
+
+console.log(checkArray([1,2,3,'a','b','c','d','e']));
