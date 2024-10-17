e6e06750b4fd3fbdf8aeafc00e74f517523ceab6
jeremyda173 <jh4017034@gmail.com>
2024-10-17 13:51:57 -0400

    chore: continue progress on course 76 and 77

--- 176/js/test4.js
+++ 176/js/test4.js
@@ -21,3 +21,49 @@ let saludo = "Hello, world";
 console.log(saludo);
 
 // Capitulo 75... No usar VAR, no siempre es la solucin y su scope es muy diferente.
+
+// Lexical scpope del curso 76
+
+function afuera() {
+  console.log("Estamos afuera");
+  const jugador = "Michael Jordan";
+    function adentro(jugador) {
+        return console.log(jugador)
+    }
+    adentro(`${jugador} 1996, ` + ` ${jugador} 1992`);
+}
+afuera();
+
+//Functions expressions
+
+const operators = function ops (a,b) {
+    const suma = a - b;
+    return console.log(`Esta es el resultado de la operacion realizada = ${suma}`);
+}
+
+operators(5,3);
+
+function suma(a, b) {
+    return a + b;
+  }
+  
+  function resta(a, b) {
+    return a - b;
+  }
+  
+  function multiplicacion(a, b) {
+    return a * b;
+  }
+  
+  function division(a, b) {
+    if (b !== 0) {
+      return a / b;
+    } else {
+      return "No se puede dividir entre cero";
+    }
+  }
+
+const theOperations = [suma, resta, multiplicacion, division];
+for (let lasFuncion of theOperations) {
+    console.log(lasFuncion(8,4));
+}
